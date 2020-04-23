hughhn/bitcoin

Implement SetType in DescriptorScriptPubKeyMan

src/wallet/scriptpubkeyman.cpp
@@ -1617,4 +1617,8 @@ uint256 DescriptorScriptPubKeyMan::GetID() const
     return id;
 }
 
-void DescriptorScriptPubKeyMan::SetType(OutputType type, bool internal) {}
+void DescriptorScriptPubKeyMan::SetType(OutputType type, bool internal)
+{
+    this->address_type = type;
+    this->internal = internal;
+}

Kino1994/bitcoin-full-history

src/wallet/scriptpubkeyman.cpp
@@ -1608,4 +1608,8 @@ uint256 DescriptorScriptPubKeyMan::GetID() const
     return uint256();
 }
 
-void DescriptorScriptPubKeyMan::SetType(OutputType type, bool internal) {}
+void DescriptorScriptPubKeyMan::SetType(OutputType type, bool internal)
+{
+    this->m_address_type = type;
+    this->m_internal = internal;
+}