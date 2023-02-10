Lajandra/bitcoin

refactor, validation: Add ChainstateManagerOpts db options
Use ChainstateManagerOpts struct to remove ArgsManager uses from validation.cpp.

This commit does not change behavior.

src/node/chainstatemanager_args.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // Copyright (c) 2022 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
@@ -37,3 +38,24 @@ std::optional<bilingual_str> ApplyArgsManOptions(const ArgsManager& args, Chains
     return std::nullopt;
 }
 } // namespace node
+||||||| parent of ee6058f7a0e (refactor, validation: Add ChainstateManagerOpts db options)
+=======
+// Copyright (c) 2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <node/chainstatemanager_args.h>
+
+#include <kernel/chainstatemanager_opts.h>
+#include <node/coins_view_args.h>
+#include <node/database_args.h>
+
+namespace node {
+void ReadChainstateManagerArgs(const ArgsManager& args, kernel::ChainstateManagerOpts& options)
+{
+    ReadDatabaseArgs(args, options.block_tree_db);
+    ReadDatabaseArgs(args, options.coins_db);
+    ReadCoinsViewArgs(args, options.coins_view);
+}
+} // namespace node
+>>>>>>> ee6058f7a0e (refactor, validation: Add ChainstateManagerOpts db options)

Kino1994/bitcoin-full-history

src/node/chainstatemanager_args.cpp
@@ -5,6 +5,9 @@
 #include <node/chainstatemanager_args.h>
 
 #include <arith_uint256.h>
+#include <kernel/chainstatemanager_opts.h>
+#include <node/coins_view_args.h>
+#include <node/database_args.h>
 #include <tinyformat.h>
 #include <uint256.h>
 #include <util/strencodings.h>
@@ -33,6 +36,10 @@ std::optional<bilingual_str> ApplyArgsManOptions(const ArgsManager& args, Chains
     if (auto value{args.GetArg("-assumevalid")}) opts.assumed_valid_block = uint256S(*value);
 
     if (auto value{args.GetIntArg("-maxtipage")}) opts.max_tip_age = std::chrono::seconds{*value};
+
+    ReadDatabaseArgs(args, opts.block_tree_db);
+    ReadDatabaseArgs(args, opts.coins_db);
+    ReadCoinsViewArgs(args, opts.coins_view);
 
     return std::nullopt;
 }