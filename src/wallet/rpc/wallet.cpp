Sjors/bitcoin

wallet: Move addhdkey logic into CWallet
Move wallet related logic from the addhdkey RPC into
CWallet::AddHDKey(), and make the RPC call the wallet method.

The wallet method returns a `util::Result<CExtPubKey>`, carrying the
master xpub on success and a translated error message on failure. The
RPC continues to enforce the unlocked-wallet precondition via
EnsureWalletIsUnlocked() and maps any wallet error to RPC_WALLET_ERROR.

Review hint:

    git show --color-moved=dimmed-zebra \
             --color-moved-ws=allow-indentation-change

src/wallet/rpc/wallet.cpp
@@ -867,19 +867,14 @@ RPCMethod addhdkey()
             std::shared_ptr<CWallet> const wallet = GetWalletForJSONRPCRequest(request);
             if (!wallet) return UniValue::VNULL;
 
-            if (wallet->IsWalletFlagSet(WALLET_FLAG_DISABLE_PRIVATE_KEYS)) {
-                throw JSONRPCError(RPC_WALLET_ERROR, "addhdkey is not available for wallets without private keys");
-            }
-
             EnsureWalletIsUnlocked(*wallet);
 
-            CExtKey hdkey;
-            if (request.params[0].isNull()) {
-                CKey seed_key = GenerateRandomKey();
-                hdkey.SetSeed(seed_key);
-            } else {
-                hdkey = DecodeExtKey(request.params[0].get_str());
-                if (!hdkey.key.IsValid()) {
+            std::optional<CExtKey> existing_key;
+
+            if (!request.params[0].isNull()) {
+                existing_key = DecodeExtKey(request.params[0].get_str());
+
+                if (!existing_key->key.IsValid()) {
                     // Check if the user gave us an xpub and give a more descriptive error if so
                     CExtPubKey xpub = DecodeExtPubKey(request.params[0].get_str());
                     if (xpub.pubkey.IsValid()) {
@@ -890,32 +885,13 @@ RPCMethod addhdkey()
                 }
             }
 
-            LOCK(wallet->cs_wallet);
-            std::string desc_str = "unused(" + EncodeExtKey(hdkey) + ")";
-            FlatSigningProvider keys;
-            std::string error;
-            std::vector<std::unique_ptr<Descriptor>> descs = Parse(desc_str, keys, error, false);
-            CHECK_NONFATAL(!descs.empty());
-            WalletDescriptor w_desc(std::move(descs.at(0)), GetTime(), 0, 0, 0);
-            if (wallet->GetDescriptorScriptPubKeyMan(w_desc) != nullptr) {
-                throw JSONRPCError(RPC_WALLET_ERROR, "HD key already exists");
-            }
-
-            auto spkm = wallet->AddWalletDescriptor(w_desc, keys, /*label=*/"", /*internal=*/false);
-            if (!spkm) {
-                throw JSONRPCError(RPC_WALLET_ERROR, util::ErrorString(spkm).original);
+            auto res = wallet->AddHDKey(existing_key);
+            if (!res) {
+                throw JSONRPCError(RPC_WALLET_ERROR, util::ErrorString(res).original);
             }
 
             UniValue response(UniValue::VOBJ);
-            const DescriptorScriptPubKeyMan& desc_spkm = spkm->get();
-            LOCK(desc_spkm.cs_desc_man);
-            std::set<CPubKey> pubkeys;
-            std::set<CExtPubKey> extpubs;
-            desc_spkm.GetWalletDescriptor().descriptor->GetPubKeys(pubkeys, extpubs);
-            CHECK_NONFATAL(pubkeys.size() == 0);
-            CHECK_NONFATAL(extpubs.size() == 1);
-            response.pushKV("xpub", EncodeExtPubKey(*extpubs.begin()));
-
+            response.pushKV("xpub", EncodeExtPubKey(*res));
             return response;
         },
     };

src/wallet/wallet.h
@@ -11,11 +11,13 @@
 #include <interfaces/chain.h>
 #include <interfaces/handler.h>
 #include <kernel/cs_main.h>
+#include <key.h>
 #include <node/types.h>
 #include <outputtype.h>
 #include <policy/feerate.h>
 #include <primitives/transaction.h>
 #include <primitives/transaction_identifier.h>
+#include <pubkey.h>
 #include <script/interpreter.h>
 #include <script/script.h>
 #include <support/allocators/secure.h>
@@ -1045,6 +1047,13 @@ public:
 
     //! Add a descriptor to the wallet, return a ScriptPubKeyMan & associated output type
     util::Result<std::reference_wrapper<DescriptorScriptPubKeyMan>> AddWalletDescriptor(WalletDescriptor& desc, const FlatSigningProvider& signing_provider, const std::string& label, bool internal) EXCLUSIVE_LOCKS_REQUIRED(cs_wallet);
+
+    //! Add an HD key to the wallet and return its master xpub.
+    //! Requires the wallet to be unlocked.
+    //! @param[in] existing_key Optional extended private key to add. If not provided,
+    //!                         a new random HD key will be generated.
+    //! @return The master xpub for the added HD key, or an error on failure.
+    util::Result<CExtPubKey> AddHDKey(const std::optional<CExtKey>& existing_key);
 
     /** Move all records from the BDB database to a new SQLite database for storage.
      * The original BDB file will be deleted and replaced with a new SQLite file.

Kino1994/bitcoin-full-history

src/wallet/rpc/wallet.cpp
@@ -864,13 +864,10 @@ RPCMethod addhdkey()
 
             EnsureWalletIsUnlocked(*wallet);
 
-            CExtKey hdkey;
-            if (request.params[0].isNull()) {
-                CKey seed_key = GenerateRandomKey();
-                hdkey.SetSeed(seed_key);
-            } else {
+            std::optional<CExtKey> hdkey;
+            if (!request.params[0].isNull()) {
                 hdkey = DecodeExtKey(request.params[0].get_str());
-                if (!hdkey.key.IsValid()) {
+                if (!hdkey->key.IsValid()) {
                     // Check if the user gave us an xpub and give a more descriptive error if so
                     CExtPubKey xpub = DecodeExtPubKey(request.params[0].get_str());
                     if (xpub.pubkey.IsValid()) {
@@ -881,32 +878,13 @@ RPCMethod addhdkey()
                 }
             }
 
-            LOCK(wallet->cs_wallet);
-            std::string desc_str = "unused(" + EncodeExtKey(hdkey) + ")";
-            FlatSigningProvider keys;
-            std::string error;
-            std::vector<std::unique_ptr<Descriptor>> descs = Parse(desc_str, keys, error, false);
-            CHECK_NONFATAL(!descs.empty());
-            WalletDescriptor w_desc(std::move(descs.at(0)), GetTime(), 0, 0, 0);
-            if (wallet->GetDescriptorScriptPubKeyMan(w_desc) != nullptr) {
-                throw JSONRPCError(RPC_WALLET_ERROR, "HD key already exists");
-            }
-
-            auto spkm = wallet->AddWalletDescriptor(w_desc, keys, /*label=*/"", /*internal=*/false);
-            if (!spkm) {
-                throw JSONRPCError(RPC_WALLET_ERROR, util::ErrorString(spkm).original);
+            auto res = wallet->AddHDKey(hdkey);
+            if (!res) {
+                throw JSONRPCError(RPC_WALLET_ERROR, util::ErrorString(res).original);
             }
 
             UniValue response(UniValue::VOBJ);
-            const DescriptorScriptPubKeyMan& desc_spkm = spkm->get();
-            LOCK(desc_spkm.cs_desc_man);
-            std::set<CPubKey> pubkeys;
-            std::set<CExtPubKey> extpubs;
-            desc_spkm.GetWalletDescriptor().descriptor->GetPubKeys(pubkeys, extpubs);
-            CHECK_NONFATAL(pubkeys.size() == 0);
-            CHECK_NONFATAL(extpubs.size() == 1);
-            response.pushKV("xpub", EncodeExtPubKey(*extpubs.begin()));
-
+            response.pushKV("xpub", EncodeExtPubKey(*res));
             return response;
         },
     };

src/wallet/wallet.h
@@ -11,11 +11,13 @@
 #include <interfaces/chain.h>
 #include <interfaces/handler.h>
 #include <kernel/cs_main.h>
+#include <key.h>
 #include <node/types.h>
 #include <outputtype.h>
 #include <policy/feerate.h>
 #include <primitives/transaction.h>
 #include <primitives/transaction_identifier.h>
+#include <pubkey.h>
 #include <script/interpreter.h>
 #include <script/script.h>
 #include <support/allocators/secure.h>
@@ -1055,6 +1057,13 @@ public:
 
     //! Add a descriptor to the wallet, return a ScriptPubKeyMan & associated output type
     util::Result<std::reference_wrapper<DescriptorScriptPubKeyMan>> AddWalletDescriptor(WalletDescriptor& desc, const FlatSigningProvider& signing_provider, const std::string& label, bool internal) EXCLUSIVE_LOCKS_REQUIRED(cs_wallet);
+
+    //! Add an HD key to the wallet and return its master xpub.
+    //! Requires the wallet to be unlocked.
+    //! @param[in] key Optional extended private key to add. If not provided,
+    //!                a new random HD key will be generated.
+    //! @return The master xpub for the added HD key, or an error on failure.
+    util::Result<CExtPubKey> AddHDKey(const std::optional<CExtKey>& key);
 
     /** Move all records from the BDB database to a new SQLite database for storage.
      * The original BDB file will be deleted and replaced with a new SQLite file.